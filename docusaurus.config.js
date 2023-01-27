ac7ec3ba1d0934789afe314387c41f97079d40ee
martodyj <115088220+martodyj@users.noreply.github.com>
2023-01-27 11:39:16 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
-  url: 'https://marrtinsdocusaurustestsssss.com',
+  url: 'https://docusaurus-2.netlify.app',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   favicon: 'img/favicon.ico',
